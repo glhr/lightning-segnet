1ae4fdc733c62fb02b0af71e7ba5502914a2c021
glhr <grenblot@gmail.com>
2021-03-29 21:50:19 +0200

    fixed enable_debug() in logger

--- utils.py
+++ utils.py
@@ -11,15 +11,18 @@ def create_folder(folder):
 
 # set up logging
 logger = logging.getLogger(__name__)
-coloredlogs.install(
-    level='INFO',
-    logger=logger,
-    fmt='[%(levelname)s] %(message)s',
-    level_styles=coloredlogs.parse_encoded_styles('spam=22;debug=28;verbose=34;info=226;notice=220;warning=202;success=118,bold;error=124;critical=background=red'))
+
+def setup_logger(level='INFO'):
+    global logger
+    coloredlogs.install(
+        level=level,
+        logger=logger,
+        fmt='[%(levelname)s] %(message)s',
+        level_styles=coloredlogs.parse_encoded_styles('spam=22;debug=28;verbose=34;info=226;notice=220;warning=202;success=118,bold;error=124;critical=background=red'))
 
 def enable_debug():
-    logger.setLevel(logging.DEBUG)
-    # return logger
+    global logger
+    setup_logger(level='DEBUG')
 
 def get_printer(depth=2):
     return pprint.PrettyPrinter(depth=depth)
