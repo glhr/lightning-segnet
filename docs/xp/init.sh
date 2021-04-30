4974a1368589625687c955847549ee88253f56be
glhr <grenblot@gmail.com>
2021-04-30 14:36:42 +0200

    init.sh

--- docs/xp/init.sh
+++ docs/xp/init.sh
@@ -3,5 +3,5 @@ python3 fusion-test.py --fusion ssma --dataset cityscapes --modalities rgb,depth
 python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-init --decoders multi --pretrained_last_layer --fusion_activ softmax
 python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-init --decoders multi --fusion_activ softmax
 python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-init --decoders single --fusion_activ softmax
-python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-rgb,d --decoders late --pretrained_last_layer --fusion_activ softmax
-python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-rgb,d --decoders late --fusion_activ softmax
+python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-init --decoders late --pretrained_last_layer --fusion_activ softmax
+python3 fusion-test.py --fusion custom --dataset cityscapes --modalities rgb,depthraw --save --bs 1 --save_xp fusion-init --decoders late --fusion_activ softmax
