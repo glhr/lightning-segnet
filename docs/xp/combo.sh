ed197cd9b219a2180723f381823750dbb403d9df
glhr <grenblot@gmail.com>
2021-05-12 14:30:30 +0200

    updated combo checkpoint

--- docs/xp/combo.sh
+++ docs/xp/combo.sh
@@ -4,14 +4,18 @@ arg=$1
 
 for dataset in freiburg cityscapes thermalvoc synthia kitti multispectralseg freiburgthermal lostfound
 do
-  checkpoint1="2021-05-10 19-34-combo-c30-sord-1,2,3-a1-logl2-rgb-epoch=49-val_loss=0.0053"
+  checkpoint1="2021-05-12 11-24-combo-c30-sord-1,2,3-a1-logl2-rgb-epoch=42-val_loss=0.0060"
   txtoutput="results/${dataset}/${xp}/txt/${checkpoint1}.txt"
 
   isInFile=$(cat "$txtoutput" | grep -c "DATALOADER:0 TEST RESULTS")
   if [ ! -f "$txtoutput" ] || [ $isInFile -eq 0 ] ; then
     mkdir -p results/$dataset/$xp/txt
     echo "Running evaluation for ${dataset} | ${checkpoint1}"
-    python3 lightning.py --dataset $dataset --bs 1 --save --save_xp $xp --save --dataset_combo_ntrain 180 --test_checkpoint "lightning_logs/${checkpoint1}.ckpt" --loss_weight > "$txtoutput" 2>&1
+    if [ -f "lightning_logs/${checkpoint1}.ckpt" ] ] ; then
+      python3 lightning.py --dataset $dataset --bs 1 --save --save_xp $xp --save --dataset_combo_ntrain 180 --test_checkpoint "lightning_logs/${checkpoint1}.ckpt" --loss_weight > "$txtoutput" 2>&1
+    else
+      echo "checkpoint ${checkpoint1} not found :( bye"
+    fi
   fi
   if [[ $arg == "overlay" ]]; then
     echo "--> generating overlays for ${dataset}"
@@ -21,14 +25,18 @@ do
   echo "--> summary for ${dataset} | ${checkpoint1}"
   tail -14 "$txtoutput"
 
-  checkpoint2="2021-05-10 22-40-combo-c3-sord-1,2,3-a1-logl2-lw-rgb-epoch=52-val_loss=0.0023"
+  checkpoint2="no"
   txtoutput="results/${dataset}/${xp}/txt/${checkpoint2}.txt"
 
   isInFile=$(cat "$txtoutput" | grep -c "DATALOADER:0 TEST RESULTS")
   if [ ! -f "$txtoutput" ] || [ $isInFile -eq 0 ] ; then
     mkdir -p results/$dataset/$xp/txt
     echo "Running evaluation for ${dataset} | ${checkpoint2}"
-    python3 lightning.py --dataset $dataset --bs 1 --save --save_xp $xp --save --dataset_combo_ntrain 180 --test_checkpoint "lightning_logs/${checkpoint2}.ckpt" --loss_weight > "$txtoutput" 2>&1
+    if [ -f "lightning_logs/${checkpoint2}.ckpt" ] ] ; then
+      python3 lightning.py --dataset $dataset --bs 1 --save --save_xp $xp --save --dataset_combo_ntrain 180 --test_checkpoint "lightning_logs/${checkpoint2}.ckpt" --loss_weight > "$txtoutput" 2>&1
+    else
+      echo "checkpoint ${checkpoint2} not found :( bye"
+    fi
   fi
   if [[ $arg == "overlay" ]]; then
     echo "--> generating overlays for ${dataset}"
