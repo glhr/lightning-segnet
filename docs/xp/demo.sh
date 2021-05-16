817d51266289312157f50202ba92f1e883823761
glhr <grenblot@gmail.com>
2021-05-16 10:46:40 +0200

    demo scripts

--- docs/xp/demo.sh
+++ docs/xp/demo.sh
@@ -1,17 +1,19 @@
-set=09; v=V000
+checkpoints=(
+  "fusionfusion-custom16rll-multi-2021-05-03 22-03-freiburgthermal-c3-sord-1,2,3-a1-logl2-lw-rgb,ir-epoch=41-val_loss=0.0016"
+  "fusionfusion-custom16rll-multi-2021-05-13 09-17-freiburgthermal-c3-sord-1,2,3-a1-logl2-lw-rgb,ir-epoch=27-val_loss=0.0016"
+)
 
-python3 fusion-test.py --fusion custom --modalities rgb,ir --save --bs 1 --save_xp demo-s$set-$v --decoders multi --test_checkpoint "lightning_logs/fusionfusion-custom16rll-multi-2021-05-03 22-03-freiburgthermal-c3-sord-1,2,3-a1-logl2-lw-rgb,ir-epoch=41-val_loss=0.0016.ckpt" --dataset kaistped --dataset_seq set$set/$v --fusion_activ softmax
-python3 fusion-test.py --fusion custom --modalities rgb,ir --save --bs 1 --save_xp demo-s$set-$v --decoders multi --test_checkpoint "lightning_logs/fusionfusion-custom16rll-multi-2021-05-03 11-30-freiburgthermal-c3-sord-1,2,3-a1-logl2-rgb,ir-epoch=39-val_loss=0.0038.ckpt" --dataset kaistped --dataset_seq set$set/$v --gpus 0
-python3 fusion-test.py --fusion custom --modalities rgb,ir --save --bs 1 --save_xp demo-s$set-$v --decoders multi --test_checkpoint "lightning_logs/fusionfusion-custom16rll-multi-2021-05-07 11-08-freiburgthermal-c3-sord-1,2,3-a1-logl2-rgb,ir-epoch=25-val_loss=0.0036.ckpt" --dataset kaistped --dataset_seq set$set/$v --gpus 0
+dataset=kaistped
+arg=$1
 
-python3 lightning.py --bs 1 --dataset kaistped --test_checkpoint "lightning_logs/2021-05-06 13-48-freiburgthermal-c13-sord-1,2,3-a1-logl2-rgb-epoch=23-val_loss=0.0037.ckpt" --save --save_xp demo-s$set-$v --modalities rgb --dataset_seq set$set/$v
-# python3 lightning.py --bs 1 --dataset kaistped --test_checkpoint "lightning_logs/2021-05-02 22-07-freiburgthermal-c13-kl-ir-epoch=56-val_loss=0.5759.ckpt" --save --save_xp demo-s$set-$v --modalities ir --dataset_seq set$set/$v
-
-python3 overlay_imgs.py --dataset kaistped --xp demo-s$set-$v --rgb --ir --model "fusionfusion-custom16rll-multi-2021-05-03 22-03-freiburgthermal-c3-sord-1,2,3-a1-logl2-lw-rgb,ir-epoch=41-val_loss=0.0016_affordances"
-
-ffmpeg -y -r 40 -f image2 -s 1920x1080 -i results/kaistped/demo-s$set-$v/overlay/kaistped%05d-demo-s$set-$v-pred_overlay.png -vcodec libx264 -crf 25  -pix_fmt yuv420p results/kaistped/kaistped-s$set-$v.mp4
-
-
-python3 overlay_imgs.py --dataset kaistped --xp demo-s$set-$v --rgb --ir --model "fusionfusion-custom16rll-multi-2021-05-03 22-03-freiburgthermal-c3-sord-1,2,3-a1-logl2-lw-rgb,ir-epoch=41-val_loss=0.0016_affordances" --model2 "2021-05-06 13-48-freiburgthermal-c13-sord-1,2,3-a1-logl2-rgb-epoch=23-val_loss=0.0037_affordances" --model3 "fusionfusion-custom16rll-multi-2021-05-07 11-08-freiburgthermal-c3-sord-1,2,3-a1-logl2-rgb,ir-epoch=25-val_loss=0.0036_affordances"
-
-python3 overlay_imgs.py --dataset kaistped --xp demo-s$set-$v --rgb --ir --model "fusionfusion-custom16rll-multi-2021-05-03 11-30-freiburgthermal-c3-sord-1,2,3-a1-logl2-rgb,ir-epoch=39-val_loss=0.0038_affordances" --model2 "2021-05-06 13-48-freiburgthermal-c13-sord-1,2,3-a1-logl2-rgb-epoch=23-val_loss=0.0037_affordances" --alpha 0.5
+for checkpoint in "${checkpoints[@]}"
+do
+  for set in 06 07 08
+  do
+    for v in V000 V001 V002 V003 V004
+    do
+      python3 fusion-test.py --bs 1 --fusion custom --modalities rgb,ir --save --save_xp demo-s$set-$v --decoders multi --fusion_activ softmax --dataset $dataset --test_checkpoint "lightning_logs/${checkpoint}.ckpt" --save --dataset_seq set$set/$v
+      python3 overlay_imgs.py --dataset $dataset --xp demo-s$set-$v --model "${checkpoint}_affordances" --rgb --ir
+    done
+  done
+done
--- docs/xp/vids.sh
+++ docs/xp/vids.sh
@@ -1,1 +1,7 @@
 ffmpeg -y -r 15 -f image2 -s 1920x1080 -i results/synthia/combo/overlay/synthiaOmni_F_%06d-combo-Rgb-pred_overlay.png -vcodec libx264 -crf 25  -pix_fmt yuv420p results/synthia/synthia-combo-test.mp4
+
+ffmpeg -y -r 40 -f image2 -s 1920x1080 -i results/kaistped/demo-s$set-$v/overlay/kaistped%05d-demo-s$set-$v-pred_overlay.png -vcodec libx264 -crf 25  -pix_fmt yuv420p results/kaistped/kaistped-s$set-$v.mp4
+
+ffmpeg -y -r 40 -f image2 -s 1920x1080 -i results/freiburthermal/thermo/overlay/kaistped%05d-demo-s$set-$v-pred_overlay.png -vcodec libx264 -crf 25  -pix_fmt yuv420p results/kaistped/kaistped-s$set-$v.mp4
+
+ffmpeg -r 8 -f image2 -pattern_type glob -i "results/freiburgthermal/${xp}/overlayRgbIr-_${checkpoint}_affordances/${dataset}*-RgbIr-pred_overlay.png" -c:v libx264 -qp 0 "results/$dataset/${checkpoint}.mp4"
