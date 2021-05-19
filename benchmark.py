510afca4182ff0ec7e6380bbcf026af198d1d1a2
glhr <grenblot@gmail.com>
2021-05-20 01:37:42 +0200

    torch profiler

--- benchmark.py
+++ benchmark.py
@@ -1,7 +1,9 @@
 import torch
-from torch import nn
 import torch.utils.benchmark as benchmark
+import torch.autograd.profiler as profiler
+
 import pickle
+import os
 from pathlib import Path
 
 from segnet import SegNet
@@ -17,6 +19,15 @@ create_folder("results/benchmark")
 model_segnet_1 = SegNet(num_classes=num_classes, n_init_features=1)
 model_segnet_2 = SegNet(num_classes=num_classes, n_init_features=2)
 model_segnet_3 = SegNet(num_classes=num_classes, n_init_features=3)
+
+input_size = (1, 1, 240, 480)
+x = 255*torch.rand(size=input_size)
+with profiler.profile(record_shapes=True, profile_memory=True) as prof:
+    with profiler.record_function("model_inference"):
+        model_segnet_1(x)
+
+# print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=1))
+print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
 
 
 mid_ssma_2 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="single", branches=2)
@@ -46,16 +57,16 @@ models = [
     "model_segnet_1",
     "model_segnet_2",
     "model_segnet_3",
-    "dual_ssma_2",
-    "mid_ssma_2",
-    "late_ssma_2",
-    "dual_ssma_3",
-    "mid_ssma_3",
-    "late_ssma_3"
+    "dual_custom_2",
+    "mid_custom_2",
+    "late_custom_2",
+    "dual_custom_3",
+    "mid_custom_3",
+    "late_custom_3"
     ]
 
-num_threads = 1
-min_run_time = 1
+num_threads = os.cpu_count()
+min_run_time = 60
 
 for model in models:
     # label and sub_label are the rows
