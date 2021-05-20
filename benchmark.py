dbf204db3a54583ceef1d43571525040fef3f4e3
glhr <grenblot@gmail.com>
2021-05-20 09:10:55 +0200

    benchmark args

--- benchmark.py
+++ benchmark.py
@@ -5,84 +5,90 @@ import torch.autograd.profiler as profiler
 import pickle
 import os
 from pathlib import Path
+from varname import varname, nameof
+
 
 from segnet import SegNet
 from fusion import FusionNet
 
 from utils import logger, create_folder
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--cuda', default=False, action="store_true")
+parser.add_argument('--runtime', default=60, type=int)
+parser.add_argument('--threads', default=os.cpu_count(), type=int)
+args = parser.parse_args()
+logger.debug(args)
+
 
 num_classes = 3
 
 logger.info(f"... setting up models ...")
 create_folder("results/benchmark")
 
-model_segnet_1 = SegNet(num_classes=num_classes, n_init_features=1)
-model_segnet_2 = SegNet(num_classes=num_classes, n_init_features=2)
-model_segnet_3 = SegNet(num_classes=num_classes, n_init_features=3)
+# input_size = (1, 1, 240, 480)
+# x = 255*torch.rand(size=input_size)
+# with profiler.profile(record_shapes=True, profile_memory=True) as prof:
+#     with profiler.record_function("model_inference"):
+#         model_segnet_1(x)
+#
+# # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=1))
+# print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
 
-input_size = (1, 1, 240, 480)
-x = 255*torch.rand(size=input_size)
-with profiler.profile(record_shapes=True, profile_memory=True) as prof:
-    with profiler.record_function("model_inference"):
-        model_segnet_1(x)
+model_dict = {
+  "model_segnet_1": SegNet(num_classes=num_classes, n_init_features=1),
+  "model_segnet_2": SegNet(num_classes=num_classes, n_init_features=2),
+  "model_segnet_3": SegNet(num_classes=num_classes, n_init_features=3),
 
-# print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=1))
-print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+  "mid_ssma_2": FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="single", branches=2),
+  "mid_custom_2": FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="single", branches=2),
 
+  "dual_ssma_2": FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="multi", branches=2),
+  "dual_custom_2": FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="multi", branches=2),
 
-mid_ssma_2 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="single", branches=2)
-mid_custom_2 = FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="single", branches=2)
-
-dual_ssma_2 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="multi", branches=2)
-dual_custom_2 = FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="multi", branches=2)
-
-late_ssma_2 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="late", branches=2)
-late_custom_2 = FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="late", branches=2)
+  "late_ssma_2": FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="late", branches=2),
+  "late_custom_2": FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="late", branches=2),
 
 
+  "mid_custom_3": FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="single", branches=3),
+  "mid_ssma_3": FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="single", branches=3),
 
-mid_custom_3 = FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="single", branches=3)
-mid_ssma_3 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="single", branches=3)
+  "dual_custom_3": FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="multi", branches=3),
+  "dual_ssma_3": FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="multi", branches=3),
 
-dual_custom_3 = FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="multi", branches=3)
-dual_ssma_3 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="multi", branches=3)
+  "late_ssma_3": FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="late", branches=3),
+  "late_custom_3": FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="late", branches=3)
 
-late_ssma_3 = FusionNet(fusion="ssma", bottleneck=16, fusion_activ="sigmoid", num_classes=num_classes, decoders="late", branches=3)
-late_custom_3 = FusionNet(fusion="custom", bottleneck=16, fusion_activ="softmax", num_classes=num_classes, decoders="late", branches=3)
+}
 
+for model_str,m in model_dict.items():
+    if args.cuda:
+        m = m.cuda()
 
 results = []
 
-models = [
-    "model_segnet_1",
-    "model_segnet_2",
-    "model_segnet_3",
-    "dual_custom_2",
-    "mid_custom_2",
-    "late_custom_2",
-    "dual_custom_3",
-    "mid_custom_3",
-    "late_custom_3"
-    ]
+num_threads = args.threads
+min_run_time = args.runtime
 
-num_threads = os.cpu_count()
-min_run_time = 60
-
-for model in models:
+for model_str in model_dict.keys():
     # label and sub_label are the rows
     # description is the column
-    channels = int(model.split("_")[-1])
+    channels = int(model_str.split("_")[-1])
     label = f'input=(1, {channels}, 240, 480)'
     input_size = (1, channels, 240, 480)
-    sub_label = model
-    x = 255*torch.rand(size=input_size)
+    sub_label = model_str
+    if args.cuda:
+        x = 255*torch.rand(size=input_size, device='cuda')
+    else:
+        x = 255*torch.rand(size=input_size)
 
-    save = f'results/benchmark/{model} {num_threads}threads {min_run_time}s.pickle'
+    save = f'results/benchmark/{model_str} {num_threads}threads {min_run_time}s.pickle'
     if not Path(save).is_file():
-        logger.info(f"-> running eval for {model}")
+        logger.info(f"-> running eval for {model_str}")
+        model = model_dict[f"{model_str}"]
         result = benchmark.Timer(
-            stmt=f'{model}(x)',
-            setup=f'from __main__ import {model}',
+            stmt=f'model(x)',
+            setup=f'from __main__ import model',
             globals={'x': x},
             num_threads=num_threads,
             label=label,
@@ -92,7 +98,7 @@ for model in models:
         with open(save, 'wb') as handle:
             pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
     else:
-        logger.info(f"-> found eval for {model}, loading pickle")
+        logger.info(f"-> found eval for {model_str}, loading pickle")
         with open(save, 'rb') as handle:
             result = pickle.load(handle)
 
